4364b1cc2efca8afdf971c3778f6b7c1438ced5a
CodingExperiments1 <124308608+CodingExperiments1@users.noreply.github.com>
2024-06-21 12:55:31 -0400

    authentication commit

---
+++ spring-todo-list/src/main/java/com/example/spring_todo_list/conf/WebSecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.spring_todo_list.conf;
+
+public class WebSecurityConfig {
+}
--- spring-todo-list/src/main/java/com/example/spring_todo_list/service/user/UserDetailService.java
+++
@@ -1,4 +0,0 @@
-package com.example.spring_todo_list.service.user;
-
-public class UserDetailService {
-}
